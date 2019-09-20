fb0aca5d31272410e52b22d91c12c4998ff293ee
Dan Fritchman <daniel.fritchman@gmail.com>
2019-09-20 14:27:45 +0200

    Chapter 12 - Command-Line

---
+++ 0012-cli/minigrep/src/main.rs
@@ -0,0 +1,23 @@
+extern crate minigrep;
+
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+
+fn main() {
+    // Collect & print argv
+    let args: Vec<String> = env::args().collect();
+    // Create a Config
+    let config = Config::new(&args)
+        .unwrap_or_else(|err| {
+            eprintln!("Problem parsing arguments: {}", err);
+            process::exit(1);
+        });
+    // And run with it
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("App error: {}", e);
+        process::exit(1);
+    };
+}
